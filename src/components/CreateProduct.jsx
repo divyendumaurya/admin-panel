848c996ab0593a96f15b8c0a5643a816411e7bf8
divyendumaurya <rajatmaurya0720@gmail.com>
2025-02-13 17:04:03 +0530

    Finalized The app
    Finalized The app with All Libraries including redux , error handling ,Toast , Loader

--- src/components/CreateProduct.jsx
+++ src/components/CreateProduct.jsx
@@ -1,208 +1,4 @@
-// src/components/CreateProduct.js
-// import React, { useState } from "react";
-// import { useDispatch } from "react-redux";
-// import { createProduct } from "../store/ProductSlice";
-// import { useNavigate } from "react-router-dom";
-// import toast from "react-hot-toast";
-// import { uploadFile } from "../store/FileUploadSlice";
-// import IsLoadingHOC from "../HOC/loader/isLoadingHOC";
-// import bgImg from "../assets/bg.jpg"
-
-// const CreateProduct = ({ setLoading }) => {
-//   const [title, setTitle] = useState("");
-//   const [price, setPrice] = useState("");
-//   const [description, setDescription] = useState("");
-//   const [categoryId, setCategoryId] = useState(1); // assuming a default category ID
-//   const [customCategory, setCustomCategory] = useState("");
-//   const [uploadedImage, setUploadedImage] = useState(null);
-//   const dispatch = useDispatch();
-//   const navigate = useNavigate();
-
-//   const handleFileUpload = async (e) => {
-//     const file = e.target.files[0];
-//     // Validate file type
-//     if (!file || !file.type.startsWith("image/")) {
-//       toast.error("Please upload a valid image file.");
-//       e.target.value = ""; // Clear the file input
-//       return; // Stop further execution
-//     }
-
-//     try {
-//       setLoading(true);
-//       const result = await dispatch(uploadFile(file)).unwrap();
-//       setUploadedImage(result.location); // Save the uploaded image URL
-//       setLoading(false);
-//       toast.success("Image uploaded successfully!");
-//     } catch (error) {
-//       setLoading(false);
-//       console.error("File upload error:", error);
-//       toast.error("Failed to upload image. Please try again.");
-//     }
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-
-//     const productData = {
-//       title,
-//       price: parseFloat(price),
-//       description,
-//       categoryId: customCategory ? customCategory : parseInt(categoryId, 10), // Use customCategory if provided
-//       images: uploadedImage ? [uploadedImage] : [],
-//     };
-
-//     console.log("Sending product data:", productData); // Debugging: log the data being sent
-
-//     dispatch(createProduct(productData)).then((result) => {
-//       if (result.error) {
-//         console.error("Error creating product:", result.error); // Debugging: log any errors
-//         console.error(result.error.message);
-//         toast.error("Failed to create product.");
-//       } else {
-//         navigate("/user/products");
-//         toast.success("Product created successfully!");
-//       }
-//     });
-//   };
-
-//   return (
-//     <>
-
-//       <section className="bg-white dark:bg-gray-900">
-//         <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
-//           <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">
-//             Add a new product
-//           </h2>
-//           <form onSubmit={handleSubmit}>
-//             <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
-//               <div className="sm:col-span-2">
-//                 <label
-//                   htmlFor="name"
-//                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-//                 >
-//                   Product Name
-//                 </label>
-//                 <input
-//                   value={title}
-//                   onChange={(e) => setTitle(e.target.value)}
-//                   type="text"
-//                   name="name"
-//                   id="name"
-//                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-//                   placeholder="Type product name"
-//                   required
-//                 />
-//               </div>
-//               <div className="w-full">
-//                 <label
-//                   htmlFor="price"
-//                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-//                 >
-//                   Price
-//                 </label>
-//                 <input
-//                   value={price}
-//                   onChange={(e) => setPrice(e.target.value)}
-//                   type="number"
-//                   name="price"
-//                   id="price"
-//                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-//                   placeholder="₹2999"
-//                   required
-//                 />
-//               </div>
-//               <div className="w-full">
-//                 <label
-//                   htmlFor="file"
-//                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-//                 >
-//                   Upload Image
-//                 </label>
-//                 <input
-//                   type="file"
-//                   name="file"
-//                   id="file"
-//                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-//                   onChange={handleFileUpload}
-//                   accept="image/*"
-//                 />
-//               </div>
-//               <div>
-//                 <label
-//                   htmlFor="category"
-//                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-//                 >
-//                   Category
-//                 </label>
-//                 <select
-//                   value={categoryId}
-//                   onChange={(e) => {
-//                     setCategoryId(parseInt(e.target.value, 10));
-//                     setCustomCategory("");
-//                   }}
-//                   id="category"
-//                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
-//                 >
-//                   <option value="">Select category</option>
-//                   <option value="1">Clothes</option>
-//                   <option value="2">Electronics</option>
-//                   <option value="3">Furniture</option>
-//                   <option value="4">Shoes</option>
-//                   <option value="5">Miscellaneous</option>
-//                 </select>
-//                 <div className="mt-4 col-span-2">
-//                   <label
-//                     htmlFor="customCategory"
-//                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-//                   >
-//                     Or type your category
-//                   </label>
-//                   <input
-//                     type="number"
-//                     id="customCategory"
-//                     value={customCategory}
-//                     onChange={(e) => {
-//                       setCustomCategory(e.target.value);
-//                       setCategoryId("");
-//                     }}
-//                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
-//                     placeholder="Type your category ID"
-//                   />
-//                 </div>
-//               </div>
-//               <div className="sm:col-span-2">
-//                 <label
-//                   htmlFor="description"
-//                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-//                 >
-//                   Description
-//                 </label>
-//                 <textarea
-//                   value={description}
-//                   onChange={(e) => setDescription(e.target.value)}
-//                   id="description"
-//                   rows="8"
-//                   className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
-//                   placeholder="Your description here"
-//                 ></textarea>
-//               </div>
-//             </div>
-//             <button
-//               type="submit"
-//               className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
-//             >
-//               Add product
-//             </button>
-//           </form>
-//         </div>
-//       </section>
-//     </>
-//   );
-// };
-
-// export default IsLoadingHOC(CreateProduct);
-
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { createProduct } from "../store/ProductSlice";
 import { useNavigate } from "react-router-dom";
@@ -211,14 +7,17 @@ import { uploadFile } from "../store/FileUploadSlice";
 import IsLoadingHOC from "../HOC/loader/isLoadingHOC";
 import bgImg from "../assets/bg.jpg";
 import { FaCloudUploadAlt, FaTrash } from "react-icons/fa";
+import { fetchCategories } from "../store/CategorySlice";
 
 const CreateProduct = ({ setLoading }) => {
   const [title, setTitle] = useState("");
   const [price, setPrice] = useState("");
   const [description, setDescription] = useState("");
   const [categoryId, setCategoryId] = useState(1);
-  const [customCategory, setCustomCategory] = useState("");
+  // const [customCategory, setCustomCategory] = useState("");
   const [uploadedImage, setUploadedImage] = useState(null);
+  const [categories, setCategories] = useState([]);
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -250,6 +49,20 @@ const CreateProduct = ({ setLoading }) => {
     setUploadedImage(null);
   };
 
+  //Fetch categories
+  useEffect(() => {
+    // Fetch categories on mount
+    const getCategories = async () => {
+      try {
+        const response = await dispatch(fetchCategories()).unwrap();
+        setCategories(response); // Assuming response is an array of categories
+      } catch (error) {
+        console.error("Failed to fetch categories:", error);
+      }
+    };
+    getCategories();
+  }, [dispatch]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -257,7 +70,7 @@ const CreateProduct = ({ setLoading }) => {
       title,
       price: parseFloat(price),
       description,
-      categoryId: customCategory ? customCategory : parseInt(categoryId, 10),
+      categoryId: parseInt(categoryId, 10),
       images: uploadedImage ? [uploadedImage.url] : [],
     };
 
@@ -378,19 +191,18 @@ const CreateProduct = ({ setLoading }) => {
                 value={categoryId}
                 onChange={(e) => {
                   setCategoryId(parseInt(e.target.value, 10));
-                  setCustomCategory("");
+                  // setCustomCategory("");
                 }}
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
               >
-                <option value="">Select category</option>
-                <option value="1">Clothes</option>
-                <option value="2">Electronics</option>
-                <option value="3">Furniture</option>
-                <option value="4">Shoes</option>
-                <option value="5">Miscellaneous</option>
+                {categories.map((category) => (
+                  <option key={category.id} value={category.id}>
+                    {category.id} - {category.name}
+                  </option>
+                ))}
               </select>
 
-              <input
+              {/* <input
                 type="text"
                 value={customCategory}
                 onChange={(e) => {
@@ -399,10 +211,10 @@ const CreateProduct = ({ setLoading }) => {
                 }}
                 className="mt-4 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                 placeholder="Or type your custom category ID"
-              />
-              <span className="text-gray-500 text-sm font-bold opacity-75">
+              /> */}
+              {/* <span className="text-gray-500 text-sm font-bold opacity-75">
                 You'll find the Category Id on HomePage in Category list option
-              </span>
+              </span> */}
             </div>
 
             <div>
